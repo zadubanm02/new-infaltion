9581c12923e2eca42a509cca835dc253fa4a6484
zadubanm02 <zadubanm02@gmail.com>
2024-02-19 21:18:08 +0100

    StatCard

--- components/StatCard.tsx
+++ components/StatCard.tsx
@@ -1,27 +1,23 @@
-import React from "react";
+import React, { ReactElement, ReactNode } from "react";
 import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
 
-const StatCard = () => {
+type Props = {
+  title: string;
+  icon?: ReactElement;
+  value: string;
+  description?: string;
+};
+
+const StatCard: React.FC<Props> = ({ title, icon, value, description }) => {
   return (
-    <Card className="w-96">
+    <Card className="w-96 my-4 mx-2">
       <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2 ">
-        <CardTitle className="text-sm font-medium">Total Revenue</CardTitle>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth="2"
-          className="h-4 w-4 text-muted-foreground"
-        >
-          <path d="M12 2v20M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6" />
-        </svg>
+        <CardTitle className="text-sm font-medium">{title}</CardTitle>
+        {icon}
       </CardHeader>
       <CardContent>
-        <div className="text-2xl font-bold">$45,231.89</div>
-        <p className="text-xs text-muted-foreground">+20.1% from last month</p>
+        <div className="text-2xl font-bold">{value}</div>
+        <p className="text-xs text-muted-foreground">{description}</p>
       </CardContent>
     </Card>
   );
