4200f7e72f340ae580c543d0b157e0e593066b29
zadubanm02 <zadubanm02@gmail.com>
2024-02-26 14:44:34 +0100

    Saved amount and count

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -1,26 +1,11 @@
 import DashboardProductList from "@/components/DashboardProductList";
-import StatCard from "@/components/StatCard";
-import { DollarSignIcon } from "lucide-react";
+
 import React from "react";
 
 const Dashboard = () => {
   return (
     <div>
       <h1 className="text-xl font-semibold mb-4">Dashboard</h1>
-      <div className="flex flex-row">
-        <StatCard
-          title="Total usetrene"
-          value="$17.99"
-          description="Zaplatis o 19% menej"
-          icon={<DollarSignIcon />}
-        />
-        <StatCard
-          title="Total usetrene"
-          value="$17.99"
-          description="Zaplatis o 19% menej"
-          icon={<DollarSignIcon />}
-        />
-      </div>
 
       <DashboardProductList />
     </div>
--- components/DashboardProductList.tsx
+++ components/DashboardProductList.tsx
@@ -4,37 +4,60 @@ import React from "react";
 import ProductCard from "./ProductCard";
 import { z } from "zod";
 import { productSchema } from "@/prisma/zod/product";
+import StatCard from "./StatCard";
+import { DollarSignIcon, ScanBarcodeIcon } from "lucide-react";
 
 type ProductSchema = z.infer<typeof productSchema>;
 
 const DashboardProductList = () => {
   const { data, error, isLoading } = useMatchedProducts();
+  const itemsInSale = data.items.length;
+  const sale = data.items.reduce((acc: number, item: ProductSchema) => {
+    return acc + (item.originalPrice - item.discountedPrice);
+  }, 0);
 
   if (isLoading) {
     return <p>Loading...</p>;
   }
   return (
-    <div className="flex flex-row flex-wrap">
-      {data &&
-        data.items.map((product: ProductSchema) => {
-          return (
-            <ProductCard
-              key={product.id}
-              id={product.id}
-              discount={product.discount}
-              store={product.store}
-              title={product.title}
-              originalPrice={product.discountedPrice}
-              discountedPrice={product.originalPrice}
-              imageUrl={product.imageUrl}
-            />
-          );
-        })}
+    <>
+      <div className="flex flex-row">
+        <StatCard
+          title="Total usetrene"
+          value={`$ ${sale}`}
+          description={`Za svoje produkty zaplatis o ${sale} eura menej`}
+          icon={<DollarSignIcon />}
+        />
+        <StatCard
+          title="Produkty v zlave"
+          value={itemsInSale}
+          description={`${itemsInSale} produktov ktore sledujes je momentalne v zlave`}
+          icon={<ScanBarcodeIcon />}
+        />
+      </div>
 
-      {/* {data && countQuery.data && filters.searchTerm === "" && (
+      <div className="flex flex-row flex-wrap">
+        {data &&
+          data.items.map((product: ProductSchema) => {
+            return (
+              <ProductCard
+                key={product.id}
+                id={product.id}
+                discount={product.discount}
+                store={product.store}
+                title={product.title}
+                originalPrice={product.discountedPrice}
+                discountedPrice={product.originalPrice}
+                imageUrl={product.imageUrl}
+              />
+            );
+          })}
+
+        {/* {data && countQuery.data && filters.searchTerm === "" && (
     <PaginationComponent nextPage={offset + 6 < countQuery.data.count} />
   )} */}
-    </div>
+      </div>
+    </>
   );
 };
 
